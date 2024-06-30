43990da3a83678561773c8102922e905422822e4
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-30 12:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
 
-	event.preventDefault();
-	
-	let form = document.getElementById("form1");
+    // Get the form element by its ID
+    var form = document.getElementById('form1');
 
-	let firstName = form.name["fname"].value;
-	let lastName = form.name["lname"].value;
-	let output = firstName+ " " + lastName;
-	alert(output);
+    // Get the values of the first name and last name input fields
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+
+    // Concatenate the names with a space in between
+    var fullName = firstName + ' ' + lastName;
+
+    // Display the full name using alert
+    alert(fullName);
 }
