a3780c20a6be0b400b1daa7b3e459d7e79ebf275
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-30 11:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
 
+	let form = document.getElementById("form1");
+
+	let firstName = form.name["fname"].value;
+	let lastName = form.name["lname"].value;
+	let output = firstName+ " " + lastName;
+	alert(output);
 }
